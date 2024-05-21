629d335b7719c03336974db3f52fe64d634bca6c
Arigatooq <McKrosh@mail.ru>
2024-05-21 08:36:07 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  publicPath: `/Rick-and-Morty/`,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
